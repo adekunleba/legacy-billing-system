43565e7f75633875a1cb09e3bae2f55868207ca5
Babatunde Adekunle <badekunle@seamfix.com>
2018-11-21 00:34:17 +0100

    Refactored Legacy Billing

--- src/main/scala/com/legacy/ServiceCharge.scala
+++ src/main/scala/com/legacy/ServiceCharge.scala
@@ -4,17 +4,43 @@ object PurchasesTypes extends Enumeration{
   val HOT, COLD = Value
 }
 
-trait Purchases
-case class Drink(name: String, amount : Money,  itemType: PurchasesTypes.Value) extends Purchases
-case class Food(name: String, amount: Money, foodType:PurchasesTypes.Value) extends Purchases
+trait Purchases {
+  val price: BigDecimal
+}
+case class Drink(name: String, amount : BigDecimal,  itemType: PurchasesTypes.Value) extends Purchases {
+  override val price: BigDecimal = amount
+}
+case class Food(name: String, amount: BigDecimal, foodType:PurchasesTypes.Value) extends Purchases {
+  override val price: BigDecimal = amount
+}
 
-class ServiceCharge(items: List[Purchases], totalAmount:Double) {
-  def getFoodCharge :Double =  items match {
-    case _ if items.forall(x => x.isInstanceOf[Drink]) =>  0
-    case _ if items.exists(x => x.isInstanceOf[Food]) => (10 * totalAmount) / 100
+
+
+class ServiceCharge(items: List[String]) {
+
+  val totalAmount: Double = makePurchaseBill
+  private def round(value: BigDecimal): Double = value.setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+  def makeItem(item: String) : Purchases = {
+    item.toLowerCase match {
+      case "cola" => Drink("cola", 0.50, PurchasesTypes.COLD)
+      case "coffee" => Drink("coffee", 1.00, PurchasesTypes.HOT)
+      case "cheese sandwish" => Food("cheese sandwish", 2.00, PurchasesTypes.COLD)
+      case "steak sandwish" => Food("stead sandwish", 4.50, PurchasesTypes.HOT)
+      case _ => throw new IllegalArgumentException(s"Item $item not found")
+    }
   }
 
-  def getHotnessCharge: List[Boolean] = items.map {
+  def makePurchaseBill :Double ={
+    round(items.map(makeItem).map(_.price).sum)
+  }
+
+  def getFoodCharge :Double =  items match {
+    case _ if items.map(makeItem).forall(x => x.isInstanceOf[Drink]) =>  0
+    case _ if items.map(makeItem).exists(x => x.isInstanceOf[Food]) => calculateHotnessCharge
+  }
+
+  def getHotnessCharge: List[Boolean] = items.map(makeItem)  {
       case d: Food if d.foodType == PurchasesTypes.HOT => true
       case _ => false
     }
@@ -22,12 +48,10 @@ class ServiceCharge(items: List[Purchases], totalAmount:Double) {
   def calculateHotnessCharge :Double =
     if (getHotnessCharge.contains(true)) {
        (20 * totalAmount) / 100
-    } else 0
-
-
+    } else (10 * totalAmount) / 100
 
   def getBill :Double = {
-    val totalServiceCharge = calculateHotnessCharge + getFoodCharge
+    val totalServiceCharge = getFoodCharge
     if (totalServiceCharge <= 20) totalAmount + totalServiceCharge else totalAmount + 20
   }
 }
--- src/test/scala/com/legacy/TestBillSpec.scala
+++ src/test/scala/com/legacy/TestBillSpec.scala
@@ -4,57 +4,47 @@ import org.scalatest._
 
 class TestBillSpec extends FlatSpec with Matchers {
 
-  val firstPurchase = List(Drink("Cola", Pence(50), PurchasesTypes.COLD))
-
-  val allPurchase :List[Purchases] = firstPurchase :+
-    Drink("Coffee", Pounds(1.00), PurchasesTypes.HOT) :+
-    Food("Cheese Sandwich", Pounds(2.00), PurchasesTypes.COLD)
-
-  val allDrinks :List[Purchases] = firstPurchase :+
-    Drink("Coffee", Pounds(1.00), PurchasesTypes.HOT) :+
-    Drink("Coffee", Pounds(1.00), PurchasesTypes.HOT)
-
-  val hotFoodPurchase:List[Purchases] = firstPurchase :+
-    Drink("Coffee", Pounds(1.00), PurchasesTypes.HOT) :+
-    Food("Cheese Sandwich", Pounds(2.00), PurchasesTypes.HOT)
+  val firstPurchase = List("Cola", "Coffee", "Cheese Sandwish")
 
 
-  val moreFoods = allPurchase ::: allDrinks ::: hotFoodPurchase :::
-  allPurchase ::: allDrinks ::: hotFoodPurchase ::: allPurchase ::: allDrinks :::
-  allPurchase ::: allDrinks ::: hotFoodPurchase ::: hotFoodPurchase ::: allDrinks :::
-  allPurchase ::: allDrinks ::: hotFoodPurchase
+  val allDrinks = List("Cola", "Coffee", "Cola", "Coffee")
+
+  val hotFoodPurchase = List("Coffee", "Steak Sandwish")
+
+
+  val moreFoods = allDrinks ::: hotFoodPurchase ::: allDrinks :::
+    hotFoodPurchase :::  allDrinks ::: allDrinks ::: hotFoodPurchase :::
+    hotFoodPurchase ::: allDrinks ::: allDrinks ::: hotFoodPurchase
        //Drink("Coffee", Pounds(1.00), PurchasesTypes.COLD)
 
   it should "show give total amount of purchases" in {
-    val totalPrice = StandardBill.makePurchaseBill(allPurchase)
+    val totalPrice = new ServiceCharge(firstPurchase).makePurchaseBill
     totalPrice shouldEqual 3.5
     totalPrice.isInstanceOf[Double] shouldEqual true
   }
 
   it should "not apply any service charge when all purchases are drinks" in {
-    val totalPrice = StandardBill.makePurchaseBill(allDrinks)
-    val serviceCharge = new ServiceCharge(allDrinks, totalPrice)
+    val serviceCharge = new ServiceCharge(allDrinks)
+    val totalPrice = serviceCharge.makePurchaseBill
     val totalAfterServiceCharge = serviceCharge.getBill
     totalAfterServiceCharge shouldEqual totalPrice
   }
 
   it should "return 10% of service charge" in {
-    val totalPrice = StandardBill.makePurchaseBill(allPurchase)
-    val serviceCharge = new ServiceCharge(allPurchase, totalPrice)
+    val serviceCharge = new ServiceCharge(firstPurchase)
     val serviceChargeAmount = serviceCharge.getFoodCharge
     serviceChargeAmount shouldEqual 0.35
   }
 
   it should "return 20% of service charge" in {
-    val totalPrice = StandardBill.makePurchaseBill(hotFoodPurchase)
-    val serviceCharge = new ServiceCharge(hotFoodPurchase, totalPrice)
+    val serviceCharge = new ServiceCharge(hotFoodPurchase)
     val hotFoodServiceCharge = serviceCharge.calculateHotnessCharge
     hotFoodServiceCharge shouldEqual 0.70
   }
 
-  it should "only add 20 pounds to service charge" in {
-    val totalPrice = StandardBill.makePurchaseBill(moreFoods)
-    val serviceCharge = new ServiceCharge(moreFoods, totalPrice)
+  it should "not add more than 20 pounds to service charge" in {
+    val serviceCharge = new ServiceCharge(moreFoods)
+    val totalPrice = serviceCharge.makePurchaseBill
     val billDifference = totalPrice - serviceCharge.getBill
     billDifference should be <= 20.0
   }
