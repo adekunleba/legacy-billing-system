84569da577f67150c8bd9b98602c3d92e7691fd7
Babatunde Adekunle <badekunle@seamfix.com>
2018-11-26 22:21:38 +0100

    A simple update

--- src/main/scala/com/legacy/ServiceCharge.scala
+++ src/main/scala/com/legacy/ServiceCharge.scala
@@ -14,14 +14,14 @@ object ServiceCharge{
   private def  purchaseItems(items: List[String]) :List[MenuItem] =
     myItems.filterKeys(items.map(_.toLowerCase).contains).values.toList
 
-  def  bill(items: List[String]): Double = {
-    purchaseItems(items).map(_.amount).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-  }
-
   lazy val myItems = Map("cola" -> MenuItem("cola", COLD , 0.50, DRINK),
     "coffee" -> MenuItem("coffee", HOT, 1.00, DRINK),
     "cheese sandwish" -> MenuItem("cheese sandwish", COLD, 2.00, FOOD),
     "steak sandwish" -> MenuItem("steak sandwish", HOT, 4.50, FOOD))
+
+  def  bill(items: List[String]): Double = {
+    purchaseItems(items).map(_.amount).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
 
   def billWithCharge(items: List[String]) :Double = {
     val thisBill = bill(items)
